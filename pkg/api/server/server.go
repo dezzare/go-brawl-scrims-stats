4a195f6acb473ff861a0ee94348067e92bd2a777
Pedro Bittencourt <djojoo@gmail.com>
2025-03-25 17:16:47 -0300

    Feat: add env var to APP_PORT

--- pkg/api/server/server.go
+++ pkg/api/server/server.go
@@ -1,7 +1,11 @@
 package server
 
-import "github.com/dezzare/go-brawl-scrims-stats/internal/router"
+import (
+	"os"
+
+	"github.com/dezzare/go-brawl-scrims-stats/internal/router"
+)
 
 func Start() {
-	router.Run(":5000")
+	router.Run(":" + os.Getenv("APP_PORT"))
 }
