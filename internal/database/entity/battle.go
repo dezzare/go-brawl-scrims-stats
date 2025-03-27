2b725e5a04ebb742fd19f998a64d2308de1490bc
Pedro Bittencourt <djojoo@gmail.com>
2025-03-27 15:41:46 -0300

    Add: json reference to Battle struct

--- internal/database/entity/battle.go
+++ internal/database/entity/battle.go
@@ -4,9 +4,10 @@ import "gorm.io/gorm"
 
 type Battle struct {
 	gorm.Model
-	Mode     Mode      `gorm:"foreignKey:Name"`
-	Map      Map       `gorm:"foreignKey:Name"`
-	Brawlers []Brawler `gorm:"many2many:brawler_events"`
-	Players  []Player  `gorm:"many2many:player_events"`
-	Result   string
+	Mode       Mode      `gorm:"foreignKey:Name"`
+	Map        Map       `gorm:"foreignKey:Name"`
+	Brawlers   []Brawler `gorm:"many2many:brawler_events"`
+	Players    []Player  `gorm:"many2many:player_events"`
+	Result     string    `json:"result"`
+	BattleTime string    `json:"battleTime"`
 }
