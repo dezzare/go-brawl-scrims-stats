d23352144884ac2b7bd1a094a32d3d955c31db7d
Pedro Bittencourt <djojoo@gmail.com>
2025-03-27 14:19:48 -0300

    Feat: brawler stats

---
+++ internal/stats/brawler.go
@@ -0,0 +1,25 @@
+package stats
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/dezzare/go-brawl-scrims-stats/internal/client"
+	"github.com/dezzare/go-brawl-scrims-stats/internal/database"
+	"github.com/dezzare/go-brawl-scrims-stats/internal/database/entity"
+)
+
+// setBrawlersBase request to API all Brawlers and save to DB
+func setBrawlersBase(c *client.Client) {
+	var model entity.Brawlers
+	if err := json.Unmarshal(c.GetBrawlers(), &model); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	db := database.Db()
+	for _, v := range model.Brawler {
+		db.Save(&v)
+	}
+
+}
--- internal/stats/stats.go
+++ internal/stats/stats.go
@@ -1,34 +1,14 @@
 package stats
 
 import (
-	"encoding/json"
-	"fmt"
-
 	"github.com/dezzare/go-brawl-scrims-stats/internal/client"
-	"github.com/dezzare/go-brawl-scrims-stats/internal/database"
-	"github.com/dezzare/go-brawl-scrims-stats/internal/database/entity"
 )
 
 func Start() {
 	c := client.ClientConn()
-	setBrawlersStats(c)
+	setBrawlersBase(c)
 	// setPlayersStats(c)
 	// setTeamStats(c)
-}
-
-// setBrawlersStats request to API all Brawlers and save to DB
-func setBrawlersStats(c *client.Client) {
-	var model entity.Brawlers
-	if err := json.Unmarshal(c.GetBrawlers(), &model); err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	db := database.Db()
-	for _, v := range model.Brawler {
-		db.Save(&v)
-	}
-
 }
 
 // func setPlayersStats(c *client.Client) {
