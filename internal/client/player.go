43fe9916950c978ae3cf569a31832b19de44cfe1
Pedro Bittencourt <djojoo@gmail.com>
2025-03-27 16:04:48 -0300

    Feat: client.GetBattleLog()

--- internal/client/player.go
+++ internal/client/player.go
@@ -10,6 +10,14 @@ func (c *Client) GetPlayer(playerTag string) (data []byte, err error) {
 	return
 }
 
+func (c *Client) GetBattleLog(playerTag string) (data []byte, err error) {
+	data, err = c.doRequest("GET", c.baseURL+"/players/"+playerTag+"/battlelog")
+	if err != nil {
+		return data, fmt.Errorf("Error getting player battlelog: %v", err)
+	}
+	return
+}
+
 func savePlayer() error {
 	return nil
 }
